4b7b8aff0bbbf0bf9dbccb9e0c721bd0eb6240fd
ZhaoLonghai <zhaolonghai1988@gmail.com>
2026-05-20 18:25:31 +0800

    test: add regression for weld coordinate world-frame semantics 555

--- test_utils.cpp
+++ test_utils.cpp
@@ -45,3 +45,4 @@ int main() {
 // v3: test at 2026-05-20, added comments and improved test output formatting  222
 // v3: test at 2026-05-20, added comments and improved test output formatting  333
 // v3: test at 2026-05-20, added comments and improved test output formatting  444
+// v3: test at 2026-05-20, added comments and improved test output formatting  
